3d158e4e8283dfa1a1a45124b91d654af0c279d3
DzhangrErdniev21D <85980107+KalmDzhan@users.noreply.github.com>
2022-10-03 12:28:25 +0300

    Add files via upload

---
+++ Erdniev2_27/Program.cs
@@ -0,0 +1,12 @@
+﻿int n, x, lastDigit, twoDigit;
+
+Console.Write("Введите n: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+lastDigit = n % 10;
+twoDigit = n / 10;
+
+x = lastDigit * 100 + twoDigit;
+
+Console.WriteLine(x);
+Console.ReadKey();
